1b19e583afe9c314bed0d18df8a1d82c5db36079
Ali-Pathan <amp.rajive@gmail.com>
2018-09-06 19:17:19 -0400

    Project 6

---
+++ src/finalconstructor/TestStudent.java
@@ -0,0 +1,11 @@
+package finalconstructor;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		SubStudent st1 = new SubStudent("Ali Pathan",3712, "3712NY_Ali Pathan");
+		st1.studentInfo();
+
+	}
+
+}
